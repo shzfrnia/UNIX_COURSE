2ef8d61f42d562e227dc9030df0365600e5f9924
Mikhail <pugumo68@gmail.com>
2020-03-13 19:18:34 +0700

    Change message when handle sig and add regex.

--- lab1/builder.sh
+++ lab1/builder.sh
@@ -19,36 +19,41 @@ function succeful_message() {
 }
 
 function handle_SIGNALS() {
-    rm -rf --$mktemp_name;
-    trap - EXIT
-    exit_with_error "User kill process."
+    rm -rf -- $mktemp_name;
+    exit_with_error "User kill process. Temporary folder was removed."
 }
 
 [ -z "$1" ] && exit_with_error 'First arguments must be src file name'
 if [ -f "$1" ]; then
-    :
+  :
 else 
-    exit_with_error "File '$1' not found"
+  exit_with_error "File '$1' not found"
 fi
-src_file_name=$1
 
-executable_file_name="runME"
+src_file_name="$1"
+OUTPUT_NAME_REGEX="s/^[[:space:]]*\/\/[[:space:]]*Output[[:space:]]*\([^ ]*\)$/\1/p"
+
+executable_file_name=$(sed -n -e "$OUTPUT_NAME_REGEX" $1 | grep -m 1 "")
+if [$executable_file_name == ""] 
+then
+  exit_with_error "Output name is not found '$src_file_name'."
+fi
 
 echo "Create temporary folder..."
 mktemp_name=$(mktemp -d -t buildermetafilesXXX) || { exit_with_error "Failed crate temporary file."; }
 succeful_message "Succeful."
 
-trap handle_SIGNALS EXIT HUP INT QUIT PIPE TERM
+trap handle_SIGNALS HUP INT QUIT PIPE TERM
 
 echo "Copying src to temporary file..."
-cp $src_file_name $mktemp_name
+cp "$src_file_name" $mktemp_name
 succeful_message "Succeful."
 
 echo "Build src file..."
 path_to_copy="$mktemp_name/$src_file_name"
-g++ -o $executable_file_name $path_to_copy 2>/dev/null || { exit_with_error "Failed compiling src file."; }
+g++ -o "$executable_file_name" "$path_to_copy" || { exit_with_error "Failed compiling src file."; }
 succeful_message "Succeful."
 
 echo "Remove temporary folder..."
-rm -rf $mktemp_name
+rm -rf -- "$mktemp_name"
 succeful_message "Succeful."
